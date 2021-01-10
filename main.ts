0040ca8f46c73e3d8fdb5590c0f9762d772c3dc7
ben-c-foster <76908598+ben-c-foster@users.noreply.github.com>
2021-01-10 09:53:48 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
-    game.showLongText("Dark corgi: Under water", DialogLayout.Full)
-    tiles.setTilemap(tilemap`level_2`)
-})
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
 	
 })
@@ -10,21 +6,35 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     BuildLevel()
 })
 function BuildLevel () {
-    x = myCorg.sprite.x
-    y = myCorg.sprite.y
     if (nextLevel == 0) {
         tiles.setTilemap(tilemap`level`)
         Corgi_go_to_red_block()
         effects.clouds.startScreenEffect()
-    } else {
+    } else if (nextLevel == 1) {
+        game.showLongText("Dark corgi: I see you have completed this fist world. but I challenge you to do a few more levels. I also have captured king corgi. defat my levels to save him ", DialogLayout.Full)
+        game.showLongText("I see its not much of a question ", DialogLayout.Full)
+        game.showLongText("now for the first level, ice jumpers  ", DialogLayout.Full)
         effects.clouds.endScreenEffect()
         effects.blizzard.startScreenEffect()
         scene.setBackgroundColor(0)
         tiles.setTilemap(tilemap`level_0`)
         Corgi_go_to_red_block()
+    } else if (nextLevel == 2) {
+        effects.clouds.endScreenEffect()
+        effects.blizzard.endScreenEffect()
+        effects.bubbles.startScreenEffect()
+        scene.setBackgroundColor(6)
+        game.showLongText("Dark corgi: Under water", DialogLayout.Full)
+        tiles.setTilemap(tilemap`level_2`)
+        Corgi_go_to_red_block()
     }
-    nextLevel += 1
+    x = myCorg.sprite.x
+    y = myCorg.sprite.y
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.rock0, function (sprite, location) {
+    myCorg.sprite.setPosition(x, y)
+    info.changeLifeBy(-1)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     myCorg.sprite.setPosition(x, y)
     info.changeLifeBy(-1)
@@ -35,12 +45,8 @@ function Corgi_go_to_red_block () {
         tiles.setTileAt(value, myTiles.transparency16)
     }
 }
-scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass2, function (sprite, location) {
-    tiles.setTilemap(tilemap`level`)
-    game.setDialogTextColor(15)
-    game.showLongText("Dark corgi: I see you have completed this fist world. but I challenge you to do a few more levels. I also have captured king corgi. defat my levels to save him ", DialogLayout.Full)
-    game.showLongText("I see its not much of a question ", DialogLayout.Full)
-    game.showLongText("now for the first level, ice jumpers  ", DialogLayout.Full)
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
+    nextLevel += 1
     BuildLevel()
 })
 let y = 0
